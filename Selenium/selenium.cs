9d3210955cdf202574d6d117b7bbea443b5e7237
Foley, David <david_foley1@dell.com>
2021-10-07 23:10:36 +0100

    update

--- Selenium/selenium.cs
+++ Selenium/selenium.cs
@@ -9,102 +9,70 @@ namespace Selenium
 {
     public class selenium
     {
+        public String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
+
         //IWebDriver webDriver = new ChromeDriver();
-
-
         [SetUp]
         public void Setup()
         {
-            // this.webAppUri = testContextInstance.Properties["webAppUri"].ToString();
             //webDriver.Navigate().GoToUrl
+            if (chromeDriverPath is null)
+            {
+                chromeDriverPath = ".";                 // for IDE
+            }
         }
 
         [Test]
         public void LowBloodPressure()
         {
-
-            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
-            if (chromeDriverPath is null)
-            {
-                chromeDriverPath = ".";                 // for IDE
-            }
-
-            using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
-            {
-
-                driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
-                driver.FindElement(By.Name("BP.Systolic")).Clear();
-                driver.FindElement(By.Name("BP.Systolic")).SendKeys("70");
-                driver.FindElement(By.Name("BP.Diastolic")).Clear();
-                driver.FindElement(By.Name("BP.Diastolic")).SendKeys("60");
-                driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
-                Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("Low Blood Pressure"));
-            }
+            using IWebDriver driver = new ChromeDriver(chromeDriverPath);
+            driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
+            driver.FindElement(By.Name("BP.Systolic")).Clear();
+            driver.FindElement(By.Name("BP.Systolic")).SendKeys("70");
+            driver.FindElement(By.Name("BP.Diastolic")).Clear();
+            driver.FindElement(By.Name("BP.Diastolic")).SendKeys("60");
+            driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
+            Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("Low Blood Pressure"));
         }
 
         [Test]
         public void IdealBloodPressure()
         {
-            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
-            if (chromeDriverPath is null)
-            {
-                chromeDriverPath = ".";                 // for IDE
-            }
-
-            using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
-            {
-                driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
-                driver.FindElement(By.Name("BP.Systolic")).Clear();
-                driver.FindElement(By.Name("BP.Systolic")).SendKeys("95");
-                driver.FindElement(By.Name("BP.Diastolic")).Clear();
-                driver.FindElement(By.Name("BP.Diastolic")).SendKeys("60");
-                driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
-                Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("Ideal Blood Pressure"));
-            }
+            using IWebDriver driver = new ChromeDriver(chromeDriverPath);
+            driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
+            driver.FindElement(By.Name("BP.Systolic")).Clear();
+            driver.FindElement(By.Name("BP.Systolic")).SendKeys("95");
+            driver.FindElement(By.Name("BP.Diastolic")).Clear();
+            driver.FindElement(By.Name("BP.Diastolic")).SendKeys("60");
+            driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
+            Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("Ideal Blood Pressure"));
         }
 
         [Test]
         public void PreHighBloodPressure()
         {
-            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
-            if (chromeDriverPath is null)
-            {
-                chromeDriverPath = ".";                 // for IDE
-            }
-
-            using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
-            {
-                driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
-                driver.FindElement(By.Name("BP.Systolic")).Clear();
-                driver.FindElement(By.Name("BP.Systolic")).SendKeys("125");
-                driver.FindElement(By.Name("BP.Diastolic")).Clear();
-                driver.FindElement(By.Name("BP.Diastolic")).SendKeys("80");
-                driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
-                Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("Pre-High Blood Pressure"));
-            }
+            using IWebDriver driver = new ChromeDriver(chromeDriverPath);
+            driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
+            driver.FindElement(By.Name("BP.Systolic")).Clear();
+            driver.FindElement(By.Name("BP.Systolic")).SendKeys("125");
+            driver.FindElement(By.Name("BP.Diastolic")).Clear();
+            driver.FindElement(By.Name("BP.Diastolic")).SendKeys("80");
+            driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
+            Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("Pre-High Blood Pressure"));
         }
 
         [Test]
         public void HighBloodPressure()
         {
-            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
-            if (chromeDriverPath is null)
-            {
-                chromeDriverPath = ".";                 // for IDE
-            }
-
-            using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
-            {
-                driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
-                driver.FindElement(By.Name("BP.Systolic")).Clear();
-                driver.FindElement(By.Name("BP.Systolic")).SendKeys("190");
-                driver.FindElement(By.Name("BP.Diastolic")).Clear();
-                driver.FindElement(By.Name("BP.Diastolic")).SendKeys("100");
-                driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
-                Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("High Blood Pressure"));
-
-                driver.Quit();
-            }
+            using IWebDriver driver = new ChromeDriver(chromeDriverPath);
+            driver.Navigate().GoToUrl("https://bloodpressure-ca-staging.azurewebsites.net");
+            driver.FindElement(By.Name("BP.Systolic")).Clear();
+            driver.FindElement(By.Name("BP.Systolic")).SendKeys("190");
+            driver.FindElement(By.Name("BP.Diastolic")).Clear();
+            driver.FindElement(By.Name("BP.Diastolic")).SendKeys("100");
+            driver.FindElement(By.XPath("//input[@value='Submit']")).Submit();
+            Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains("High Blood Pressure"));
+            driver.Quit();
         }
     }
 }
