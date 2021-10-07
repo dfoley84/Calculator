bd28043376ad239f3b8d0e5e6d45065bae250b1f
Foley, David <david_foley1@dell.com>
2021-10-07 14:29:58 +0100

    Add project files.

---
+++ BPCalculator.Tests/unitTestBloodPressure.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using BPCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BPCalculator.Tests
+{
+    [TestFixture]
+    public class unitTestBloodPressure
+    {
+        [Test]
+        public void LowBloodPressure()
+        {
+            BloodPressure bloodpressure = new BloodPressure()
+            {
+                Systolic = 70,
+                Diastolic = 40
+            };
+            BPCategory category = bloodpressure.Category;
+            Assert.AreEqual(BPCategory.Low, category);
+        }
+        [Test]
+        public void IdealBloodPressure()
+        {
+            BloodPressure bloodpressure = new BloodPressure()
+            {
+                Systolic = 90,
+                Diastolic = 70
+            };
+            BPCategory category = bloodpressure.Category;
+            Assert.AreEqual(BPCategory.Ideal, category);
+        }
+
+        [Test]
+        public void PreHighBloodPressure()
+        {
+            BloodPressure bloodpressure = new BloodPressure()
+            {
+                Systolic = 120,
+                Diastolic = 81
+            };
+            BPCategory category = bloodpressure.Category;
+            Assert.AreEqual(BPCategory.PreHigh, category);
+        }
+
+        [Test]
+        public void HighBloodPressure()
+        {
+            BloodPressure bloodpressure = new BloodPressure()
+            {
+                Systolic = 140,
+                Diastolic = 91
+            };
+            BPCategory category = bloodpressure.Category;
+            Assert.AreEqual(BPCategory.High, category);
+        }
+    }
+}
---
+++ BPCalculator/BloodPressure.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BPCalculator
+{
+    // BP categories
+    public enum BPCategory
+    {
+        [Display(Name="Low Blood Pressure")] Low,
+        [Display(Name="Ideal Blood Pressure")]  Ideal,
+        [Display(Name="Pre-High Blood Pressure")] PreHigh,
+        [Display(Name ="High Blood Pressure")]  High,
+        [Display(Name = "No Blood Pressure Found")] Unknown //Throw Expection is Breaking the Code 
+    };
+
+    public class BloodPressure
+    {
+        public const int SystolicMin = 70;
+        public const int SystolicMax = 190;
+        public const int DiastolicMin = 40;
+        public const int DiastolicMax = 100;
+
+        [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
+        public int Systolic { get; set; }                       // mmHG
+
+        [Range(DiastolicMin, DiastolicMax, ErrorMessage = "Invalid Diastolic Value")]
+        public int Diastolic { get; set; }                      // mmHG
+
+        // calculate BP category
+        public BPCategory Category
+        {
+            get
+            {
+                // implement as part of project
+                //throw new NotImplementedException("not implemented yet");        
+                if ((Systolic >= 70 && Systolic < 90) && (Diastolic >= 40 && Diastolic <= 60))
+                {
+                    return BPCategory.Low;
+                }
+
+                else if ((Systolic >= 90 && Systolic < 120) && (Diastolic >= 60 && Diastolic <= 80))
+                {
+                    return BPCategory.Ideal;
+                }
+
+                else if ((Systolic >= 120 && Systolic <= 140) && (Diastolic >= 80 && Diastolic <= 90))
+                {
+                    return BPCategory.PreHigh;
+                }
+
+                else if ((Systolic >= 140 && Systolic <= 190) && (Diastolic >= 90 && Diastolic <= 100))
+                {
+                    return BPCategory.High;
+                }
+
+                else
+                {
+                    return BPCategory.Unknown;
+                }
+            }
+        }
+    }
+}
